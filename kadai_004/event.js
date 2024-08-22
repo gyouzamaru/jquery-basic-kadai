2629e7756e244444038626e18848a5a13734ff9c
gyouzamaru <ami1225.2000@gmail.com>
2024-08-22 20:05:33 +0900

    課題提出1

---
+++ kadai_004/event.js
@@ -0,0 +1,13 @@
+$(window).on({
+  'load': () => {
+    console.log('loadイベントが発生しました');
+},
+});
+
+$(function () {
+  $(window).on({
+  'scroll': () => {
+    console.log('scrollイベントが発生しました');
+  },
+  });
+});
